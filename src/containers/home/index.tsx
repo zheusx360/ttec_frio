fdd45adfff932e5bfb26f0292c36295d41412d55
Rodrigo <rodrigo@kyte.com.br>
2022-06-04 14:49:20 -0300

    Ajustes /Home /WhatsAppButton

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -12,7 +12,7 @@ export function Home() {
         <div className={styles.column}>
           <div className={styles.row}>
             <div className={styles.content}>
-              <img src="/images/ImagesCarousel/satisfacao.jpg" alt="Satisfação" width={400} height={350} />
+              <img src="/images/ImagesCarousel/satisfacao.jpg" alt="Satisfação" width={'100%'} height={'100%'} />
             </div>
             <div className={styles.contentText}>
               <span>Excelência no Atendimento</span><br />
@@ -32,34 +32,27 @@ export function Home() {
               <p>&bull; Equipamentos modernos</p>
             </div>
             <div className={styles.content}>
-              <img src="/images/ImagesCarousel/freezer6.jpg" alt="Técnologia" width={400} height={350} />
+              <img src="/images/ImagesCarousel/freezer6.jpg" alt="Técnologia" width={'100%'} height={'100%'} />
             </div>
           </div>
         </div>
 
       </div>
       <div className={styles.sectionContainer}>
-        <section className={styles.sectionTop}>
-          <div className={styles.imageSection}>
-            <span>
-              <img src="/images/ImagesCarousel/qualidade.jpg" alt="Eficiencia" width={420} height={350} />
-            </span>
-            <div className={styles.imageText}>
-              <p>Qualidade e agilidade</p>
-              <div>
-                <span>
-                  Estamos preparados com o que há de melhor no mercado para entregar o serviço desejado com muita qualidade e agilidade, garantindo assim a melhor entrega no menor prazo. solicite um orçamento e se surpreenda com o que temos para te oferecer.
-                  <div>
-                    <Button type={5} text={"SOLICITAR ORÇAMENTO"} onClick={() => Router.push('/orcamento')}/>
-                  </div>
-                </span>
-              </div>
+        <div className={styles.controlContent}>
+        <div className={styles.Lcontent}>
+          <img src="/images/ImagesCarousel/qualidade.jpg" alt="Eficiencia" width={'100%'} height={'100%'}/>
+        </div>
+        <div className={styles.Rcontent}>
+        <p>Qualidade e agilidade</p>
+        <span>
+            Estamos preparados com o que há de melhor no mercado para entregar o serviço desejado com muita qualidade e agilidade, garantindo assim a melhor entrega no menor prazo. solicite um orçamento e se surpreenda com o que temos para te oferecer.
+            <div style={{marginTop:'3%'}}>
+              <Button type={5} width={280} text={"SOLICITAR ORÇAMENTO"} onClick={() => Router.push('/orcamento')}/>
             </div>
-            <div></div>
-          </div>
-        </section>
-        <section className={styles.sectionSub}>
-        </section>
+        </span>
+        </div>
+      </div>
       </div>
     </div>
   )
