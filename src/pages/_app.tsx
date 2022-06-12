f3bff482e357eec7fe89c83b59c081824a91d7be
Rodrigo <rodrigo@kyte.com.br>
2022-06-11 23:51:25 -0300

    verificação do google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,29 @@ import '../styles/global.scss'
 import { Header, HeaderMobile } from '../components'
 import Footer from '../components/footer/footer'
 import { WhatsAppButton } from '../components/buttons/whatsappButton'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-  <>
-    <header>
-      <title>TTEC FRIO - Câmaras frigoríficas</title>
-      <meta name="google-site-verification" content="TQrIYboCQV1vHbVKDskGx4K0tU2RiLF_nnzKKtYWkFs" />
-    </header>
-    <WhatsAppButton/>
-    <Header/>
-    <HeaderMobile/>
-    <Component {...pageProps} />
-    <Footer/>
-  </>
+    <>
+      <Head>
+        <title>TTECFRIO CÂMARAS FRIGORÍFICAS</title>
+        <meta name='description' content='ttec frio câmaras frigoríficas = manutenção, instalação e suporte' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name="google-site-verification" content="TQrIYboCQV1vHbVKDskGx4K0tU2RiLF_nnzKKtYWkFs" />
+      </Head>
+      <header>
+        <title>TTEC FRIO - Câmaras frigoríficas</title>
+        <meta name="google-site-verification" content="TQrIYboCQV1vHbVKDskGx4K0tU2RiLF_nnzKKtYWkFs" />
+      </header>
+      <WhatsAppButton />
+      <Header />
+      <HeaderMobile />
+      <Component {...pageProps} />
+      <Footer />
+    </>
   )
 
 }
